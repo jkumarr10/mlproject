bbcd8acaffb7af9c6658c82e77a3b91cf9bf8727
jkumarr10 <jitthenkumarr29@gmail.com>
2024-07-02 19:02:57 -0400

    Add .DS_Store to .gitignore and remove it from the index

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+E_DOT = '-e .'
+
+def get_requirements(file_path:str)-> List[str]:
+    ''' 
+    This function will return the list of requirements.txt of all packages needed
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n", " ") for req in requirements]
+        
+        if E_DOT in requirements:
+            requirements.remove(E_DOT)
+            
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author= 'jkumar10',
+    author_email= 'jitthenkumarr29@gmail.com', 
+    packages= find_packages(),
+    install_requires = get_requirements('requirements.txt')
+    
+)
