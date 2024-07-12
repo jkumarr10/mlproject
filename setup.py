28a8478be5dfcbd602277dcd5549c40dc43424f5
jkumarr10 <jitthenkumarr29@gmail.com>
2024-07-12 15:35:16 -0400

    logging and exception

--- setup.py
+++ setup.py
@@ -10,11 +10,12 @@ def get_requirements(file_path:str)-> List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        [req.replace("\n", " ") for req in requirements]
+        requirements = [req.replace("\n", " ") for req in requirements]
         
         if E_DOT in requirements:
             requirements.remove(E_DOT)
-            
+    
+    return requirements
 
 setup(
     name = 'mlproject',
